441b90e9a88ed7785b03f13065596a7457047e63
GVasilyev <vasilyev.georgy@gmail.com>
2019-04-30 15:03:47 +0300

    add fsr

---
+++ robotis_device/include/robotis_device/fsr.h
@@ -0,0 +1,21 @@
+#ifndef ROBOTIS_DEVICE_SENSOR_H_
+#define ROBOTIS_DEVICE_SENSOR_H_
+
+#include <map>
+#include <string>
+#include <stdint.h>
+
+#include "device.h"
+#include "sensor_state.h"
+#include "control_table_item.h"
+
+namespace robotis_framework
+{
+
+class FSR : public Sensor
+{
+
+}
+
+
+#endif /* ROBOTIS_DEVICE_SENSOR_H_ */
